a4237a77e8797e8fe33171343320ef45c9d30ea2
alperentrkn <48105284+alperentrkn@users.noreply.github.com>
2019-06-09 18:32:33 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+
+const App = () => <PersonList/>;
+
+const PersonList = () => {  
+  const people = [
+    {
+      img:22,
+      name:"John",
+      job:"developer"
+    },
+    {
+      img:34,
+      name:"Bob",
+      job:"designer"
+    },
+    {
+      img:56,
+      name:"Peter",
+      job:"artist"
+    }
+  ];
+  
+  
+  return (<section>
+    <Person person = {people[0]}/>;
+    <Person person = {people[1]}>
+      Burası bir deneme yazısıdır...
+    </Person>;
+    <Person person = {people[2]}/>;
+  </section>)
+};
+
+const Person = (props) =>{
+  const {img, name, job} = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  
+  return (<div className="person">
+    <img src={url} alt=""/>
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </div>
+  </div>);
+};
+
+
+
+export default App;
